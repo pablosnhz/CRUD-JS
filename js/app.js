157cd62bd2ddeacc9c9caa7a1bf2587b1a69ebed
PabloSnhz <pablosanhuezaaa@gmail.com>
2023-11-11 17:43:34 -0300

    localStorage

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
-import datos from '../data/data.json' assert {type: 'json'};
 import { Gift } from './clases.js';
+import { cargaDeDatos } from './funciones.js'; 
+
+let datos = [];
 
 const cuerpoTabla = document.querySelector('#cuerpo-tabla');
 const myModal = new bootstrap.Modal(document.getElementById('modalGift'))
@@ -32,13 +34,15 @@ const giftUpdate = (e) => {
     datos[index].imagen = document.querySelector('#imagenModal').value;
 
 
+    localStorage.setItem('datos', JSON.stringify(datos));
     cargarTabla()
-
     myModal.hide();
 }
 
 const cargarTabla = () => {
     
+    datos = JSON.parse(localStorage.getItem('datos')); // LOCAL STORAGE
+
     cuerpoTabla.innerHTML = ''; // limpia el form para nuevos datos reset()
 
     datos.map((item)=> {
@@ -75,6 +79,8 @@ const agregarGift = (event) => {
 
     datos.push(new Gift(id, gift, tipo, tiempo, precio, imagen));
     document.querySelector("#formGift").reset(); // limpia cambios luego de subir los datos
+    
+    localStorage.setItem('datos', JSON.stringify(datos)); // LOCAL STORAGE
     cargarTabla();
 };
 
@@ -85,12 +91,14 @@ window.borrarGift = (id) => {
     let validar = confirm(`Esta seguro/a que quiere eliminar la gift card ${datos[index].gift}?`)
 
     if(validar){
-        datos.splice(index, 1)
-        cargarTabla()
+        datos.splice(index, 1);
+        localStorage.setItem('datos', JSON.stringify(datos));
+        cargarTabla();
     }
 
 }
 
+cargaDeDatos()
 cargarTabla()
 
 document.querySelector('#formGift').addEventListener('submit', agregarGift);
